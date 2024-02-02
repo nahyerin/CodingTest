6dff0808e8a948157f30677ddbd8fcc66a804e31
nahyerin <149887357+nahyerin@users.noreply.github.com>
2024-02-03 00:04:19 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        for(int i = 0; i<s.length(); i++){
+            if(s.length()%2 == 0){
+                answer = s.substring((s.length()/2)-1, (s.length()/2)+1);
+            }else {
+                answer = s.substring(s.length() / 2, (s.length()/2)+1);
+            }
+        }
+        
+        return answer;
+    }
+}
