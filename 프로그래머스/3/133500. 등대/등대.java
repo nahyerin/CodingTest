e0e838546f605fa2b00330736264011fb0144101
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-02-14 14:55:10 +0900

    [level 3] Title: 등대, Time: 5.78 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -0,0 +1,39 @@
+import java.util.*;
+//트리를 이용한 DFS, 전체 노드 연결 상태를 루트가 1인 트리라고 생각
+class Solution {
+    public int N, result = 0;
+    public List<List<Integer>> map = new ArrayList<>();
+
+    public int dfs(int cur, int before) {
+        
+        if (map.get(cur).size() == 1 && map.get(cur).get(0) == before)
+            return 1;
+
+        int tmpRes = 0;
+        for (int i = 0; i < map.get(cur).size(); i++) {
+            int next = map.get(cur).get(i);
+            if (next == before) continue;
+            tmpRes += dfs(next, cur);
+        }
+
+		if (tmpRes == 0) 
+        	return 1;
+        
+        result++;
+        return 0;
+    }
+
+    public int solution(int n, int[][] lighthouse) {
+        
+        N = n;
+        for (int i = 0; i <= N; i++)
+            map.add(new ArrayList<>());
+        for (int i = 0; i < lighthouse.length; i++) {
+            map.get(lighthouse[i][0]).add(lighthouse[i][1]);
+            map.get(lighthouse[i][1]).add(lighthouse[i][0]);
+        }
+
+        dfs(1, 0);
+        return result;
+    }
+}
