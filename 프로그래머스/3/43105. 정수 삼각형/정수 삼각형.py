cd7041ab499c62045f9b598b0c1a2ab8c525d14e
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-03-02 00:42:10 +0900

    [level 3] Title: 정수 삼각형, Time: 33.84 ms, Memory: 14.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,12 @@
+# 아래에서 위로 올라가기
+def solution(triangle):
+
+    floor = len(triangle) - 1  # N층의 인덱스
+
+    while floor > 0:  # N, N-1,...2, 1
+        for i in range(floor):  # N번째 인덱스엔 0~N-> N+1개의 숫자가 있음
+            # 바로 위층의 칸에 아래칸의 두개중 큰값을 더해줌
+            triangle[floor-1][i] += max(triangle[floor][i], triangle[floor][i+1])
+        floor -= 1  # 층하나 올라가기
+
+    return triangle[0][0]
