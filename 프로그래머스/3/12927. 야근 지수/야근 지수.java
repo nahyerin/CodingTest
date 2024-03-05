7ecc1efb07496d5185ea8f40bd474689f7579af5
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-03-05 16:58:57 +0900

    [level 3] Title: 야근 지수, Time: 117.09 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        // 내림차순 우선순위 큐 선언
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        // 큐에 배열값 넣기
+        for (int i = 0; i < works.length; i++) {
+            queue.add(works[i]);
+        }
+        
+        while (n > 0) {
+            // 가장 큰 수가 0이면 탐색중단
+            if (queue.peek() == 0) {
+                break;
+            }
+            queue.add(queue.poll() - 1);
+            n--;
+        }
+        
+        // 큐에 있는 값 제곱해서 더하기
+        while (!queue.isEmpty()) {
+            answer += Math.pow(queue.poll(), 2);
+        }
+        return answer;
+    }
+}
