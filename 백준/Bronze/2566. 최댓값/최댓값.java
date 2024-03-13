dad70cee053bfa8e205e2934e055434b2fe9a12d
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-03-13 10:56:46 +0900

    [Bronze III] Title: 최댓값, Time: 144 ms, Memory: 15964 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -4,13 +4,15 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+   
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token;
 
+        // 9x9 크기의 배열을 선언
         int arrays[][] = new int[9][9];
 
-
+        // 입력된 숫자 배열에 저장
         for(int i = 0; i < 9; i++){
             token = new StringTokenizer(br.readLine());
             for(int j = 0; j < 9; j++){
@@ -19,19 +21,29 @@ public class Main {
         }
         br.close();
 
-        int max = 0; 
-        int row = 0; 
-        int col = 0; 
+        // 최댓값을 저장할 변수 초기화
+        int max = 0;
 
+        // 배열 순회하며 최댓값 찾기
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 if(max < arrays[i][j]){
                     max  = arrays[i][j];
-                    row = i;
-                    col = j;
                 }
             }
         }
-        System.out.println(max + "\n" + (row+1) + " " + (col+1) + " ");
+
+        // 최댓값 출력
+        System.out.println(max);
+
+        // 최댓값의 위치 출력
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(arrays[i][j] == max){
+                    System.out.println((i+1) + " " + (j+1));
+                    return; // 최댓값의 위치를 찾으면 종료
+                }
+            }
+        }
     }
 }
