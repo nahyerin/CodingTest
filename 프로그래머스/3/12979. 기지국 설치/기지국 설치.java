052329824cc6eb703ff15b9a4cbd8c68bd003a82
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-03-27 15:53:13 +0900

    [level 3] Title: 기지국 설치, Time: 0.73 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0, sectionLength;
+
+        for (int i = 0; i <= stations.length; i++) {
+            if(i == 0){//구간 시작 ~ 첫번째 기지국
+                sectionLength = stations[i] - 1 - w;
+            } else if (i == stations.length) {//마지막 기지국 ~ 구간 끝
+                sectionLength = n - stations[i - 1] - w;
+            } else {//기지국들 사이
+                sectionLength = stations[i] - stations[i - 1] - 2 * w - 1;
+            }
+
+            if (sectionLength >= 0) {
+                answer += sectionLength / (2 * w + 1);
+
+                if (sectionLength % (2 * w + 1) != 0) {
+                    ++answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
