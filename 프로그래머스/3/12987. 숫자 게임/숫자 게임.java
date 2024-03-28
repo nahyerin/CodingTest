6a384560afddb70ff0993c6bfb1cb5581951f132
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-03-28 17:49:10 +0900

    [level 3] Title: 숫자 게임, Time: 65.33 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0, temp = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i=0; i<A.length; i++){
+            for(int j=temp; j<B.length; j++){
+                if(B[j]>A[i]){
+                    answer++;
+                    temp = j+1;
+                    break;
+                }
+            }           
+        }
+        
+        return answer;
+    }
+}
