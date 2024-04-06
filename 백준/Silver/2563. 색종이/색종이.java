94bdcd49e233ffbb16664e1da7a28ea2742378b9
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-04-06 18:36:35 +0900

    Update 색종이.java

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//boolean 타입 이중 배열 -> 100, 100인 흰색 도화지
+//boolean 타입 이중 배열 -> 100, 100인 흰색
 //색종이 갯수를 입력 받은만큼 반복문, 반복문 안에서는 색종이와 벽에서의 x 거리와 y 거리를 입력받고 이중 for문을 생성
 //입력받은 x에서 색종이의 크기를 더한 x + 10까지 반복하고, y는 y + 10까지 반복하도록 하여 boolean이 false인 것만 true로 바꿔준다
 //백지 도화지에 색종이를 붙이지만 겹치는 부분은 자동으로 배제되어 색종이 영역의 넓이 구하기
