91d5dcdb789ac658e38fc46eb23a5d579a79071e
hyeeerin <149887357+nahyerin@users.noreply.github.com>
2024-04-06 18:39:10 +0900

    Update 분수찾기.java

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -7,7 +7,7 @@ public class Main {
         int X = Integer.parseInt(br.readLine());
         br.close();
         int squareCount = 1; // 해당 대각선(순회하는 대각선)에 있는 칸
-        int squareSum =  0; // 해당 대각선의 전 대각선 칸을 누적 더하기 해서 저장
+        int squareSum =  0; // 해당 대각선의 전 대각선 칸을 누적 더하기 저장
         while(true){
             if(X <= squareSum + squareCount){
                 if(squareCount % 2 == 1){ 
@@ -15,7 +15,6 @@ public class Main {
                     break;
                 }else{ //(해당 대각선의 칸의 개수가 짝수, 분자+분모(T) = 홀수)
                     //대각선의 개수가 짝수인 범위 내에서는 아래쪽(↙︎︎) 으로 순회 -> 분자 증가, 분모 감소
-                    // ︎홀수와 반대
                     System.out.println((X - squareSum) + "/" + (squareCount-(X - squareSum -1)));
                     break;
                 }
